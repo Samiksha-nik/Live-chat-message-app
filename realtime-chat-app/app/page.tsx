7aa6281aa12c7fd48a89745899215d68d4e6b71f
Samiksha-nik <nikamsamiksha43@gmail.com>
2026-02-24 19:36:55 +0530

    routing issue solved

--- realtime-chat-app/app/page.tsx
+++ realtime-chat-app/app/page.tsx
@@ -1,8 +1,8 @@
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
-export default function HomePage() {
-  const { userId } = auth();
+export default async function HomePage() {
+  const { userId } = await auth();
 
   if (userId) {
     redirect("/chat");
@@ -10,4 +10,3 @@ export default function HomePage() {
 
   redirect("/sign-in");
 }
-
